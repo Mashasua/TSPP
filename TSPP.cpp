86a361628bd4ca067d158cde7a3b06956f251003
Mashasua <101455195+Mashasua@users.noreply.github.com>
2022-05-27 19:50:06 +0300

    0
    0

--- TSPP.cpp
+++ TSPP.cpp
@@ -1,23 +1,15 @@
 ﻿// TSPP.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
-
+#define _CRT_SECURE_NO_WARNINGS
+#pragma once
+#include <string>
+#include <stdio.h>
 #include <iostream>
 
 int main()
 {
     std::cout << "Hello World!\n";
 }
-
-
-
-
-
-
-
-
-
-
-
 
 
 
